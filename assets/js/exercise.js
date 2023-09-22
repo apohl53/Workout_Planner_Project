308ffa0c9a6826535d5d14896e1e678c4cc7dcd3
Rodolfo <rodolfoespinosa01@gmail.com>
2023-09-22 14:46:10 -0400

    90% done

--- assets/js/exercise.js
+++ assets/js/exercise.js
@@ -23,13 +23,16 @@ function closeModal() {
 
 function radioButton() {
   var userInput = document.querySelector('input[name="part"]:checked');
+
   var val = userInput.value;
+
 
 
   if (val === "lowerBody") {
     var randLower = Math.floor(Math.random() * (lowerBodyLength - 1) + 1);
     var lowerString = exerciseOptions[0].bodyParts[randLower];
     localStorage.setItem('userChoice', lowerString);
+
     getStorage();
     closeModal();
   }
@@ -49,7 +52,9 @@ function radioButton() {
     closeModal();
   }
 
-  if (!val) {
+
+
+  if (val == '') {
     alert('Let us know what body section you want to work on!');
     return;
   }
@@ -77,6 +82,17 @@ var exerciseOptions = [
   }
 ]
 
+var userExercises =
+{
+  prevLower: [],
+  prevCore: [],
+  prevUpper: []
+
+}
+
+
+
+var nxtcount = 0;
 var lowerBodyLength = exerciseOptions[0].bodyParts.length;
 var coreLength = exerciseOptions[1].bodyParts.length;
 var upperBodyLength = exerciseOptions[2].bodyParts.length;
@@ -95,9 +111,23 @@ var test2 = $('#firstSection')
 
 function getStorage() {
   var getLocal = localStorage.getItem('userChoice')
-  console.log(getLocal);
+
   apiCall(getLocal);
+
 }
+
+
+function getData() {
+  return localStorage.getItem('userChoice') || [];
+}
+
+
+function getObject() {
+  var obj = JSON.parse(localStorage.getItem('object')) || [];
+  console.log(obj)
+
+}
+
 
 
 
@@ -105,6 +135,7 @@ function getStorage() {
 function apiCall(getLocal) {
   // below is the API connection
   var muscle = getLocal;
+  nxtcount += 1;
   $.ajax({
     method: 'GET',
     url: 'https://api.api-ninjas.com/v1/exercises?muscle=' + muscle,
@@ -112,18 +143,37 @@ function apiCall(getLocal) {
     contentType: 'application/json',
     success: function (result) {
 
-      var count = Math.floor(Math.random() * 10)
+      var count = Math.floor(Math.random() * 10);
+      secondSection.innerHTML = "";
       test.append('<p class = "testing">' + result[count].name +
         '<br>' + result[count].type + '<br>' + result[count].muscle +
         '<br>' + result[count].equipment +
         '<br>' + result[count].difficulty + '</p>')
+      if (muscle == 'abductors' || muscle == 'adductors' || muscle == 'calves' || muscle == 'glutes' || muscle == 'hamstrings' || muscle == 'quadriceps') {
+
+        userExercises.prevLower.push(result[count].name);
+        localStorage.setItem('LowerBody', JSON.stringify(userExercises.prevLower));
+
+
+      }
+      else if (muscle == 'biceps' || muscle == 'chest' || muscle == 'forearms' || muscle == 'lats' || muscle == 'lower_back' || muscle == 'middle_back' || muscle == 'neck' || muscle == 'traps' || muscle == 'triceps') {
+        userExercises.prevUpper.push(result[count].name);
+        localStorage.setItem('Upperbody', JSON.stringify(userExercises.prevUpper));
+
+
+      }
+      else {
+        userExercises.prevCore.push(result[count].name);
+        localStorage.setItem('Core', JSON.stringify(userExercises.prevCore));
+
+
+      }
 
       var tweb = $('.testing');
       tweb.append('<p>' + result[count].instructions + '</p>')
-      console.log(count)
-      console.log(result[count]);
 
 
+      localStorage.setItem('object', JSON.stringify(userExercises));
     },
     error: function ajaxError(jqXHR) {
       console.error('Error: ', jqXHR.responseText);
@@ -135,9 +185,19 @@ function apiCall(getLocal) {
 
 
 
+getObject();
+
 var getStartedBTN = document.getElementById('getStartedBTN');
 getStartedBTN.addEventListener('click', openModal);
 
 var closeGetStarted = document.getElementById('closeX')
 closeGetStarted.addEventListener('click', closeModal);
+
+
+
+
+
+
+
+
 
