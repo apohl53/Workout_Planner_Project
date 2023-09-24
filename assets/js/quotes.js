c2af47867d390b291828cd9bcbe1f46c483a3de1
dd <140358105+ddurandev@users.noreply.github.com>
2023-09-24 19:21:49 -0400

    save-hero

--- assets/js/quotes.js
+++ assets/js/quotes.js
@@ -1,20 +1,20 @@
 var quote = $(".quotes");
 var muscle = "triceps";
 var category = "fitness";
-$.ajax({
-  method: "GET",
-  url: "https://api.api-ninjas.com/v1/exercises?muscle=" + muscle,
-  headers: { "X-Api-Key": "bQQv3qWq5kHalNjh19VAPQ==OdFpPSD3HrbB6vz5" },
-  contentType: "application/json",
-  success: function (result) {
-    console.log(result[0]);
-    var count = Math.floor(Math.random() * 10);
-    console.log(count);
-    quote.append("<p>" + result[count].name + "</p>");
-    quote.append("<h2>" + result[count].difficulty + "</h2>");
-    quote.append("<p>" + result[count].instructions + "</p>");
-  },
-});
+// $.ajax({
+//   method: "GET",
+//   url: "https://api.api-ninjas.com/v1/exercises?muscle=" + muscle,
+//   headers: { "X-Api-Key": "bQQv3qWq5kHalNjh19VAPQ==OdFpPSD3HrbB6vz5" },
+//   contentType: "application/json",
+//   success: function (result) {
+//     console.log(result[0]);
+//     var count = Math.floor(Math.random() * 10);
+//     console.log(count);
+//     quote.append("<p>" + result[count].name + "</p>");
+//     quote.append("<h2>" + result[count].difficulty + "</h2>");
+//     quote.append("<p>" + result[count].instructions + "</p>");
+//   },
+// });
 $.ajax({
   method: "GET",
   url: "https://api.api-ninjas.com/v1/quotes?category=" + category,
